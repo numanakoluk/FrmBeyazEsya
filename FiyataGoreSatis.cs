c29156ebeaad4d692d66fd991e467ab04e00f85c
numanakoluk <numanakoluk01@gmail.com>
2022-01-02 13:12:09 +0300

    Updated

--- FiyataGoreSatis.cs
+++ FiyataGoreSatis.cs
@@ -21,7 +21,19 @@ namespace FrmBeyazEsya
         SatisDB _satisDB = new SatisDB();
         private void FiyataGoreSatis_Load(object sender, EventArgs e)
         {
-           dataGridView1.DataSource = _satisDB .FiyatAralik();
+            dataGridView1.DataSource = _satisDB.FiyatAralik();
+            GereksizGizle(); //Bu şekilde yapmasam model oluşturmak zorunda kalacaktım.
+        }
+
+        private void GereksizGizle()
+        {
+            dataGridView1.Columns["SatisID"].Visible = false;
+            dataGridView1.Columns["UrunID"].Visible = false;
+            dataGridView1.Columns["MusteriID"].Visible = false;
+            dataGridView1.Columns["MusteriAd"].Visible = false;
+            dataGridView1.Columns["MusteriSoyad"].Visible = false;
+            dataGridView1.Columns["MusteriSehir"].Visible = false;
+            dataGridView1.Columns["Adet"].Visible = false;
         }
     }
 }
